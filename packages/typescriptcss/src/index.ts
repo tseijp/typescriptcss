6c3307d7ea90b240d09764c807341eaa86335c43
tseijp <_@tsei.jp>
2026-06-17 13:58:02 +0900

    chore: wip (#4)

--- packages/typescriptcss/src/index.ts
+++ packages/typescriptcss/src/index.ts
@@ -1,4 +1,4 @@
-import { color, dark as dark_, isNum, length, media, merge, numeric, px as px_, read, scope, set, side, space, token, withScope, x4 } from './style'
+import { color, dark as dark_, isNum, length, media, merge, numeric, px as px_, read, scope, set, side, space, token, withScope, x4 } from './utils'
 import { RuntimeStyle, Chain, Utility } from './types'
 scope('flex', { col: set({ flexDirection: 'column' }), nowrap: set({ flexWrap: 'nowrap' }), row: set({ flexDirection: 'row' }), wrap: set({ flexWrap: 'wrap' }) })
 scope('items', { center: set({ alignItems: 'center' }), end: set({ alignItems: 'flex-end' }), start: set({ alignItems: 'flex-start' }), stretch: set({ alignItems: 'stretch' }) })
